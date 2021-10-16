22378607ba518ca1fdccb336e177320833fde7ed
Francisco170594 <francisco941705@gmail.com>
2021-10-16 10:37:14 -0500

    Create appy.py file and routes with flask

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
+@app.route("/about")
+def about():
+    print("Server received request for 'about' page...")
+    return "Welcome to my 'about' page"
