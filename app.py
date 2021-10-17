841c5d022942b18127b767d1f3c2010875ac653f
Francisco170594 <francisco941705@gmail.com>
2021-10-17 05:50:41 -0500

    Add tobs, stations and start temp routes

--- app.py
+++ app.py
@@ -1,41 +1,11 @@
-# from flask import Flask
-# app = Flask(__name__)
-# @app.route('/')
-# def hello_world():
-#     return 'Hello world'
-
-# @app.route("/about")
-# def about():
-#     print("Server received request for 'about' page...")
-#     return "Welcome to my 'about' page"
-
-import datetime as dt
-import numpy as np
-import pandas as pd
-
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-
-from flask import Flask, jsonify
-
-engine = create_engine("sqlite:///hawaii.sqlite")
-Base = automap_base()
-Base.prepare(engine, reflect=True)
-Measurement = Base.classes.measurement
-Station = Base.classes.station
-session = Session(engine)
-
+from flask import Flask
 app = Flask(__name__)
-@app.route("/")
-def welcome():
-    return (
-    '''
-    Welcome to the Climate Analysis API!
-    Available Routes:
-    /api/v1.0/precipitation
-    /api/v1.0/stations
-    /api/v1.0/tobs
-    /api/v1.0/temp/start/end
-    ''')
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
+@app.route("/about")
+def about():
+    print("Server received request for 'about' page...")
+    return "Welcome to my 'about' page"
+
